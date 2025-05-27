6981080334375b3353ed4ae1139e6881a1a7833d
Dylan <bollarodylan@gmail.com>
2025-05-27 21:24:51 +0200

    sharepoint download file & verification of excel

--- backend-sharepoint/graph.py
+++ backend-sharepoint/graph.py
@@ -1,9 +1,10 @@
-import asyncio
-import datetime
-import json
-
+import os
+import shutil
 import aiohttp
 import requests
+
+
+name_downloaded_files = "downloaded_files"
 
 
 async def get_app_token(credential, graph_scope):
@@ -18,20 +19,43 @@ async def create_headers(credential, graph_scope):
         "Content-Type": "application/json",
     }
 
+
 async def get_drive_id(credential, graph_scope, config):
     headers = await create_headers(credential, graph_scope)
     async with aiohttp.ClientSession() as session:
         async with session.get(
-            f"{config['azure']['ENDPOINT']}/sites",
+            f"{config['azure']['ENDPOINT']}/sites/{config['azure']['sharepointId']}/drive",
             headers=headers,
         ) as response:
             drive_info = await response.json()
             return drive_info["id"]
 
 
+async def get_files_in_sharepoint_folder(id, credential, graph_scope, config):
+    drive_id = await get_drive_id(credential, graph_scope, config)
+    headers = await create_headers(credential, graph_scope)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            f"{config['azure']['ENDPOINT']}/drives/{drive_id}/items/{id}",
+            headers=headers,
+        ) as response:
+            childrens = await response.json()
+            return childrens
+    return None
+        
+async def downloading_excel_files_from_sharepoint(id, credential, graph_scope, ms_config):
+    shutil.rmtree(name_downloaded_files, ignore_errors=True, onerror=None)
+    os.makedirs(os.path.join(name_downloaded_files), exist_ok=True)
 
+    files_in_sharepoint = await get_files_in_sharepoint_folder(id, credential, graph_scope, ms_config)
 
-async def get_folder_id(credential, graph_scope, config, subfolder=""):
+    response = requests.get(files_in_sharepoint["@microsoft.graph.downloadUrl"])
+    save_to_path = os.path.join(name_downloaded_files, files_in_sharepoint["id"]+".xlsx")
+    
+    with open(save_to_path, "wb") as f:
+        f.write(response.content)
+        
+async def get_folder_by_search_with_name(credential, graph_scope, config,full=False):
     drive_id = await get_drive_id(credential, graph_scope, config)
     headers = await create_headers(credential, graph_scope)
     async with aiohttp.ClientSession() as session:
@@ -40,93 +64,18 @@ async def get_folder_id(credential, graph_scope, config, subfolder=""):
             headers=headers,
         ) as response:
             folder = await response.json()
-            print(folder)
-            # return folder["id"]
+            files = []    
+            if full:
+                return folder
+            
+            for item in folder["value"]:
+                file = {}
+                if "name" in item and item["name"].endswith(".xlsx") and item not in files:
+                    file["name"] = item["name"]
+                    file["id"] = item["id"]
+                    file["lastModifiedDateTime"] = item["lastModifiedDateTime"]
+                    files.append(file)
+            return files
 
-
-async def get_files_in_sharepoint_folder(credential, graph_scope, config, subfolder=""):
-    drive_id = await get_drive_id(credential, graph_scope, config)
-    folder_id = await get_folder_id(credential, graph_scope, config)
-    return
-    headers = await create_headers(credential, graph_scope)
-    async with aiohttp.ClientSession() as session:
-        async with session.get(
-            f"{config['azure']['ENDPOINT']}/drives/{drive_id}/items/{folder_id}/children{ f'/{subfolder}' if subfolder else ''}",
-            headers=headers,
-        ) as response:
-            childrens = await response.json()
-            return childrens["value"]
-
-
-# async def upload_file_in_sharepoint_folder(
-#     credential, graph_scope, config, name_file, buffer
-# ):
-#     drive_id = await get_drive_id(credential, graph_scope, config)
-#     folder_id = await get_folder_id(credential, graph_scope, config)
-#     headers = await create_headers(credential, graph_scope)
-
-#     async with aiohttp.ClientSession() as session:
-#         async with session.put(
-#             f"{config['azure']['ENDPOINT']}/drives/{drive_id}/items/{folder_id}:/{name_file}:/content",
-#             headers=headers,
-#             data=buffer,
-#         ) as response:
-#             return response
-
-
-# async def move_all_files_to_a_folder(credential, graph_scope, config):
-#     new_folder_id = await creating_new_folder(credential, graph_scope, config)
-#     drive_id = await get_drive_id(credential, graph_scope, config)
-#     headers = await create_headers(credential, graph_scope)
-
-#     files_in_sharepoint = await get_files_in_sharepoint_folder(
-#         credential, graph_scope, config
-#     )
-
-#     files_in_sharepoint_list = [
-#         f for f in files_in_sharepoint if "file" in f and f["name"].endswith("xlsx")
-#     ]
-
-#     for children in files_in_sharepoint_list:
-#         payload = {
-#             "parentReference": {"id": new_folder_id},
-#             "name": children["name"],
-#         }
-#         async with aiohttp.ClientSession() as session:
-#             async with session.patch(
-#                 f"{config['azure']['ENDPOINT']}/drives/{drive_id}/items/{children['id']}",
-#                 headers=headers,
-#                 data=json.dumps(payload),
-#             ) as response:
-#                 if response.status in [200]:
-#                     print(
-#                         f"The file {children['name']} have been moved to the new folder"
-#                     )
-
-
-# async def creating_new_folder(credential, graph_scope, config):
-#     drive_id = await get_drive_id(credential, graph_scope, config)
-#     folder_id = await get_folder_id(credential, graph_scope, config, "OLD")
-#     headers = await create_headers(credential, graph_scope)
-
-#     timestamp = datetime.datetime.now().strftime("%d_%B_%Y_%H%M%S")
-#     name_new_folder = f"curriculum_{timestamp}".lower()
-
-#     payload = {
-#         "name": name_new_folder,
-#         "folder": {},
-#         "@microsoft.graph.conflictBehavior": "rename",
-#     }
-
-#     async with aiohttp.ClientSession() as session:
-#         async with session.post(
-#             f"{config['azure']['ENDPOINT']}/drives/{drive_id}/items/{folder_id}/children",
-#             headers=headers,
-#             data=json.dumps(payload),
-#         ) as response:
-#             if response.status in [201]:
-#                 print(f"Folder {name_new_folder} created.")
-#                 folder_id = await get_folder_id(
-#                     credential, graph_scope, config, f"OLD/{name_new_folder}"
-#                 )
-#                 return folder_id
+async def old_get_files_in_sharepoint_folder(credential, graph_scope, config, full=False):
+    return await get_folder_by_search_with_name(credential, graph_scope, config, full)
--- backend-sharepoint/main.py
+++ backend-sharepoint/main.py
@@ -1,12 +1,17 @@
 import asyncio
+from collections import defaultdict
 from configparser import ConfigParser
+import json
+import os
+from typing import List, Optional
+import pandas as pd
 
 from azure.identity.aio import ClientSecretCredential
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from msgraph import GraphServiceClient
 
-from graph import get_files_in_sharepoint_folder
+from graph import downloading_excel_files_from_sharepoint, old_get_files_in_sharepoint_folder
 
 ms_config = ConfigParser()
 ms_config.read(["msgraph.cfg"])
@@ -24,7 +29,6 @@ client = GraphServiceClient(credentials=credential, scopes=[graph_scope])
 
 app = FastAPI()
 
-
 origins = [
     "http://localhost:3000",
 ]
@@ -36,14 +40,55 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
+    
 @app.get("/get-files-in-sharepoint-folder")
-def read_root():
-    return asyncio.run(
-        get_files_in_sharepoint_folder(credential, graph_scope, ms_config)
-    )
+async def fn_get_files_in_sharepoint_folder():
+    return await old_get_files_in_sharepoint_folder(credential, graph_scope, ms_config)
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
+@app.get("/sheet-names")
+def sheet_names(path):
+    sheet_names = pd.ExcelFile(path)
+    return sheet_names.sheet_names
+
+@app.post("/sheet-information")
+async def sheet_information(id: Optional[str] = None):
+    await downloading_excel_files_from_sharepoint(id, credential, graph_scope, ms_config)
+    path = f"./downloaded_files/{id}.xlsx"
+    
+    sheets = sheet_names(path)
+    
+    all_sheets = {}
+    errors = []
+    
+    for sheet in sheets:
+        df = pd.read_excel(path, sheet_name=sheet)
+        df = df.fillna("")
+        dd = defaultdict(list)
+        all_sheets[sheet] = df.to_dict('records', into=dd)
+        
+    for sheet in all_sheets:
+        if sheet in ["raw_list", "analysis"]:
+            continue
+        
+        error = {"sheet": sheet}
+        
+        for dict in all_sheets[sheet]:
+            try:
+                if not len(dict["Members "].split(";")) > 0:
+                    error["message"] = f"Error in Alias {dict["Alias"]} on the col Members, check separator , or ;"
+                    errors.append(error)
+                    
+                if "," in dict["Members "]:
+                    error["message"] = f"Error in Alias {dict["Alias"]} on the col Members, check separator ,"
+                    errors.append(error)
+                
+                if not len(dict["Owner "].split(";")) > 0:
+                    error["message"] = f"Error in Alias {dict["Alias"]} on the col Owner, check separator , or ;"
+                    errors.append(error)
+                    
+                if "," in dict["Owner "]:
+                    error["message"] = f"Error in Alias {dict["Alias"]} on the col Owner, check separator ,"
+                    errors.append(error)
+            except:
+                errors.append(error)
+    return errors
